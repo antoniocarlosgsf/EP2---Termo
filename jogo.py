36d6c7d4814bd1e3c997f0aac5fdd1d4f2f4cf0f
antoniocarlosgsf <antoniocarlos171201@gmail.com>
2023-11-11 20:05:58 -0300

    Aplicando diferentes mensagens para desempenhos diferentes

--- jogo.py
+++ jogo.py
@@ -10,7 +10,7 @@ def valida_palpite(string, tamanho_sorteada):
     if string == 'desisto':
         return 'desistiu'
     if len(string) != tamanho_sorteada:
-        return 'apenas palavras de 5 letras'
+        return f'apenas palavras de {dificuldade} letras'
     elif string not in palavras_do_jogo:
         return 'palavra desconhecida'
  
@@ -32,13 +32,44 @@ amarela = ANSI.color_text(93)
 escura = ANSI.color_text(90) 
 reset = ANSI.color_text()
 
-
+#Interface inicial
+print(' =========================== ')
+print('|                           |')
+print('| Bem-Vindo ao Insper Termo |')
+print('|                           |')
+print(' =========================== ')
+print()
+print('Comandos: desisto')
+print()
+print('Regras:')
+print()
+print(' - Suas tentativas aumentam conforme a sua dificuldade;')
+print(' - A cada tentativa as letras serão coloridas da seguinte forma: ')
+print(f'. {azul} azul {reset} : a letra está na posição correta;')
+print(f'. {amarela} amarela {reset}: a palavra tem a letra, mas ela não está na posição correta;')
+print(f'. {escura} cinza {reset}: a palavra não tem a letra.')
+print(' - Os acentos são ignorados;')
+print(' - As palavras podem possuir letras repetidas.')
+print()
 print('Níveis: Facíl (5 letras)\n        Médio (6 letras)\n        Difícil (7 letras)')
 print()
 
-dificuldade = int(input('Com quantas letras deseja jogar? '))
-palavras_do_jogo = filtra(palavras, 5)
+perguntar_novamente = True
+while perguntar_novamente:
+    dificuldade = int(input('Com quantas letras deseja jogar? '))
+    if dificuldade == 5 or dificuldade == 6 or dificuldade == 7:
+        perguntar_novamente = False
+        print()
+        print('Sorteando uma palavra...')
+        print('Já tenho uma palavra! Tente adivinhá-la!')
+        print()
+        print()
+    else:
+        print('Resposta inválida')
+        print('Escolha entre 5, 6 ou 7')
+        print()
 
+palavras_do_jogo = filtra(palavras, 5)
 
 if dificuldade == 5:
     #Iniciando o jogo
@@ -115,7 +146,24 @@ if dificuldade == 5:
                 print(' --- --- --- --- ---')
                 if palpite == sorteada:
                     tentativas_feitas = 6 - tentativas
-                    print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                    if tentativas_feitas == 6:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Foi por pouco em?')
+                    elif tentativas_feitas == 5:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Você é muito bom')
+                    elif tentativas_feitas == 4:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Que demais, você é muito inteligente')
+                    elif tentativas_feitas == 3:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Com apenas 3 tentativas? Acho que está muito fácil para você!')
+                    elif tentativas_feitas == 2:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Você foi certeiro! Parabéns!')
+                    elif tentativas_feitas == 1:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Você só pode ser o Professor Xavier, Incrível!')
                     print()
                     ganhou = True
                     break
@@ -191,23 +239,45 @@ elif dificuldade == 6:
                 tentativas -= 1
                 #Printando a interface do jogo
                 print(' --- --- --- --- ---')
-                print(f'| {lista_colorida[0][0]} | {lista_colorida[0][1]} | {lista_colorida[0][2]} | {lista_colorida[0][3]} | {lista_colorida[0][4]} | {lista_colorida[0][5]} |')
+                print(f'| {lista_colorida[0][0]} | {lista_colorida[0][1]} | {lista_colorida[0][2]} | {lista_colorida[0][3]} | {lista_colorida[0][4]} | {lista_colorida[0][5]} | {lista_colorida[0][6]} |')
                 print(' --- --- --- --- --- ---')
-                print(f'| {lista_colorida[1][0]} | {lista_colorida[1][1]} | {lista_colorida[1][2]} | {lista_colorida[1][3]} | {lista_colorida[1][4]} | {lista_colorida[1][5]} |')
+                print(f'| {lista_colorida[1][0]} | {lista_colorida[1][1]} | {lista_colorida[1][2]} | {lista_colorida[1][3]} | {lista_colorida[1][4]} | {lista_colorida[1][5]} | {lista_colorida[1][6]} |')
                 print(' --- --- --- --- --- ---')
-                print(f'| {lista_colorida[2][0]} | {lista_colorida[2][1]} | {lista_colorida[2][2]} | {lista_colorida[2][3]} | {lista_colorida[2][4]} | {lista_colorida[2][5]} |')
+                print(f'| {lista_colorida[2][0]} | {lista_colorida[2][1]} | {lista_colorida[2][2]} | {lista_colorida[2][3]} | {lista_colorida[2][4]} | {lista_colorida[2][5]} | {lista_colorida[2][6]} |')
                 print(' --- --- --- --- --- ---')
-                print(f'| {lista_colorida[3][0]} | {lista_colorida[3][1]} | {lista_colorida[3][2]} | {lista_colorida[3][3]} | {lista_colorida[3][4]} | {lista_colorida[3][5]} |')
+                print(f'| {lista_colorida[3][0]} | {lista_colorida[3][1]} | {lista_colorida[3][2]} | {lista_colorida[3][3]} | {lista_colorida[3][4]} | {lista_colorida[3][5]} | {lista_colorida[3][6]} |')
                 print(' --- --- --- --- --- ---')
-                print(f'| {lista_colorida[4][0]} | {lista_colorida[4][1]} | {lista_colorida[4][2]} | {lista_colorida[4][3]} | {lista_colorida[4][4]} | {lista_colorida[4][5]} |')
+                print(f'| {lista_colorida[4][0]} | {lista_colorida[4][1]} | {lista_colorida[4][2]} | {lista_colorida[4][3]} | {lista_colorida[4][4]} | {lista_colorida[4][5]} | {lista_colorida[4][6]} |')
                 print(' --- --- --- --- --- ---')
-                print(f'| {lista_colorida[5][0]} | {lista_colorida[5][1]} | {lista_colorida[5][2]} | {lista_colorida[5][3]} | {lista_colorida[5][4]} | {lista_colorida[5][5]} |')
+                print(f'| {lista_colorida[5][0]} | {lista_colorida[5][1]} | {lista_colorida[5][2]} | {lista_colorida[5][3]} | {lista_colorida[5][4]} | {lista_colorida[5][5]} | {lista_colorida[5][6]} |')
                 print(' --- --- --- --- --- ---')
-                print(f'| {lista_colorida[6][0]} | {lista_colorida[6][1]} | {lista_colorida[6][2]} | {lista_colorida[6][3]} | {lista_colorida[6][4]} | {lista_colorida[6][5]} |')
+                print(f'| {lista_colorida[6][0]} | {lista_colorida[6][1]} | {lista_colorida[6][2]} | {lista_colorida[6][3]} | {lista_colorida[6][4]} | {lista_colorida[6][5]} | {lista_colorida[6][6]} |')
                 print(' --- --- --- --- --- ---')
+                print(f'| {lista_colorida[7][0]} | {lista_colorida[7][1]} | {lista_colorida[7][2]} | {lista_colorida[7][3]} | {lista_colorida[7][4]} | {lista_colorida[7][5]} | {lista_colorida[7][6]} |')
                 if palpite == sorteada:
                     tentativas_feitas = 7 - tentativas
-                    print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                    if tentativas_feitas == 7:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Você é muito bom')
+                    elif tentativas_feitas == 6:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Você é craque!')
+                    elif tentativas_feitas == 5:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Acertou em cheio')
+                    elif tentativas_feitas == 4:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Certeiro, você nasceu para jogar termo!')
+                    elif tentativas_feitas == 3:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print(f'Perfeito, você é muito bom!!!')
+                    elif tentativas_feitas == 2:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Você é um gênio!!!')
+                    elif tentativas_feitas == 1:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Não é possível, você só pode ser o Cristiano Ronaldo do Termo!!!')
+
                     print()
                     ganhou = True
                     break
@@ -299,7 +369,31 @@ elif dificuldade == 7:
                 print(' --- --- --- --- --- ---')
                 if palpite == sorteada:
                     tentativas_feitas = 8 - tentativas
-                    print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                    if tentativas_feitas == 8:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Você é bom em... ')
+                    elif tentativas_feitas == 7:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Você é muito bom')
+                    elif tentativas_feitas == 6:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Estou impressionado, você é absurdamente bom!!')
+                    elif tentativas_feitas == 5:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Vou precisar criar um novo nível só para você!!')
+                    elif tentativas_feitas == 4:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Você é LENDÁRIO!!!!!')
+                    elif tentativas_feitas == 3:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Você só pode ser o Messi do Termo')
+                    elif tentativas_feitas == 2:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('IMPOSSÍVEL!!! VOCÊ É O MELHOR DE TODOS')
+                    elif tentativas_feitas == 1:
+                        print(f'*** Parabéns! Você acertou depois de {tentativas_feitas} tentativas!')
+                        print('Você só pode estar de rack, não é possível')
+                        print('VOCÊ É O MAIOR DA HISTÓRIA!!!!!!!')
                     print()
                     ganhou = True
                     break
